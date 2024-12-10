ce6c941a7eafa6152cfad04dd332c1120d5a6604
Enio Cocja <eniococja123@gmail.com>
2024-12-10 14:58:54 +0100

    Methode Add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        double a = 10;
+        double b = 5;
+
+        System.out.println("Addition: " + add(a, b));
+
     }
+
+    public static double add(double x, double y) {
+        return x + y;
+    }
+
+
 }
