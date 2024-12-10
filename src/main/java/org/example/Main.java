f0a1ad3673e3e62b05ce0ad0c4e784fac2fc3ede
Armend <armend_tahiraga@outlook.com>
2024-12-10 15:12:59 +0100

    Test subtract method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(add(2, 3));
+        System.out.println(subtract(2, 3));
     }
 
     public static double add(double x, double y) {
